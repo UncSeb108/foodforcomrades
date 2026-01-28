da8b7907506780f102d6deb03288badb6eabbb32
preacherBoy <preacherboy@gmail.com>
2026-01-28 13:23:37 +0300

    fix: add localhost origins to CORS for development

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,12 @@ app.use(
       "https://foodforcomrade.com",
       "http://localhost:3000",
       "http://localhost:3001",
+      "http://localhost:4000",
+      "http://localhost:8000",
       "http://127.0.0.1:3000",
-      "http://127.0.0.1:3001"
+      "http://127.0.0.1:3001",
+      "http://127.0.0.1:4000",
+      "http://127.0.0.1:8000"
     ],
     methods: ["GET", "POST"],
     credentials: true,
